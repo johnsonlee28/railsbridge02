1d47fe7e0ead1d919e64f3063c610833a3757979
Johnsonlee <johnsonlee28@gmail.com>
2017-04-02 18:59:33 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+
+  end
+  root 'topics#index'
+  resources :abouts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
